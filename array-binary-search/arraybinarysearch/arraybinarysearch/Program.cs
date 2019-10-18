a1a1fdfc962590655155dfcf50dd041fc8140d46
darrikpuetz <darrikpuetz@gmail.com>
2019-10-17 18:48:12 -0700

    updating binary search

---
+++ array-binary-search/arraybinarysearch/arraybinarysearch/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace arraybinarysearch
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number for the amount of numbers in the array:");
+            try
+            {
+                string input = Console.ReadLine();
+                int valueInput = int.Parse(input);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public static
+    }
+}
---
+++ array-binary-search/arraybinarysearch/arraybinarysearchTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using static arraybinarysearch.Program;
+
+namespace arraybinarysearchTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
